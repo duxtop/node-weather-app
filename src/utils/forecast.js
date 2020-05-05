61a131c2e181a267c1767550d1d58b8f3920380d
Alok Singh Vats <duxtop@gmail.com>
2020-05-06 01:37:24 +0530

    Humidity Added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast= (latitude, longitude, callback) => {
         }else if (body.error){
             callback('Unable to find your address', undefined)
         }else{
-            callback(undefined, 'It is currently '+ body.current.temperature +' degress out. It feels like '+ body.current.feelslike +' degress out.')
+            callback(undefined, 'It is currently '+ body.current.temperature +' degress out. It feels like '+ body.current.feelslike +' degress out. The humidity is '+ body.current.humidity +' degress out. :)')
         }
     })
 
